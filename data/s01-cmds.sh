3a78fbb6abbc00d81ed9a4197ece5c7d2cd860d8
Ing. Jan Trdlička, Ph.D <jan.trdlicka@fit.cvut.cz>
2020-11-24 16:39:37 +0100

    Upload New File

---
+++ data/s01-cmds.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "--------------------"
+date
+echo "  - Users:      $( finger | tail -n+2 | wc -l )"
+echo "  - Processes:  $( ps -e | tail -n+2 | wc -l )"
+echo "  - Threads:    $( ps -eL | tail -n+2 | wc -l )"
+echo "--------------------"
